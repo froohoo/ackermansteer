cbc9e99dfb06a129c49283c8ae369a7d649ea53e
froohoo <froohoo@gmail.com>
2019-03-20 11:11:14 -0500

    Broke methods out of class def.

--- src/ackermansteer.cpp
+++ src/ackermansteer.cpp
@@ -6,35 +6,50 @@
 
 namespace gazebo
 {
-   class ModelPush : public ModelPlugin
-   {
-      public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
-      {
-         //store the pointer to the model
-         this->model= _parent;
+   // Wheel order follows cartestion quadrant numbering
+   // when x axis indicates primary direction of motion
+   enum{ FL, RL, RR, FR };
 
-         // Listen to the update event. This event is broadcast every 
-         // simulation iteration.
-         this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-               std::bind(&ModelPush::OnUpdate, this));
-      }
+   class AckermanSteer : public ModelPlugin
+   {
+      public: 
+         AckermanSteer();
+         ~AckermanSteer();
+         void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/);
+         void OnUpdate();
+      private: 
+         physics::ModelPtr model;                // Pointer to the model
+         event::ConnectionPtr updateConnection;  // Pointer to the update event connection
+   
+   };
+   
+   // Constructor
+   AckermanSteer::AckermanSteer() {}
+
+   // Destructor
+   AckermanSteer::~AckermanSteer() {}
+
+   // Required Load method:
+   void AckermanSteer::Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
+   {
+      //store the pointer to the model
+      this->model= _parent;
+        
+      // Listen to the update event. This event is broadcast every 
+      // simulation iteration.
+      this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+            std::bind(&AckermanSteer::OnUpdate, this));
+   }
 
    //Called by the world update start event
-   public: void OnUpdate()
+   void AckermanSteer::OnUpdate()
    {
       // Apply a small linear velocity to the model
       this->model->SetLinearVel(ignition::math::Vector3d(.3, 0, 0));
    }
 
-   // Pointer to the model
-   private: physics::ModelPtr model;
-
-   // Pointer to the update event connection
-   private: event::ConnectionPtr updateConnection;
-
-   };
 
    //Register this plugin with the simulator
-   GZ_REGISTER_MODEL_PLUGIN(ModelPush)
+   GZ_REGISTER_MODEL_PLUGIN(AckermanSteer)
 }
 
